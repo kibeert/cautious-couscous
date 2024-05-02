f25a2268ecadc065252815794b8e98145b47826d
kibeert <ckibertkoech@gmail.com>
2024-05-02 11:25:29 +0300

    added openweathermap api and modified index.html

--- src/main/views.py
+++ src/main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        # Remove spaces around the query parameters in the URL
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=9ac0ad1926c1469b0c2dcd9dbadffebb'
+
+        source = urllib.request.urlopen(url).read() 
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']), 
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']), 
+            "temp": str(list_of_data['main']['temp']) + 'k', 
+            "pressure": str(list_of_data['main']['pressure']), 
+            "humidity": str(list_of_data['main']['humidity']), 
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, 'main/index.html', data)
